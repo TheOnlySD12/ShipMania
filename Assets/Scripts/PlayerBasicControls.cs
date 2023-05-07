4dd8e54f8dd8f67bba74812d3049c6e2a8320651
Rares_Hapaianu <rares.hapaianu@gmail.com>
2023-05-07 19:19:19 +0300

    Collision

--- Assets/Scripts/PlayerBasicControls.cs
+++ Assets/Scripts/PlayerBasicControls.cs
@@ -5,35 +5,23 @@ using UnityEngine;
 public class PlayerBasicControls : MonoBehaviour
 {
     Rigidbody2D body;
-<<<<<<< HEAD
-=======
+
     [SerializeField] float gunTimer;
     float firerate = 1; //cu cat e mai mare, cu atat mai repede trage
     GameObject clone;
     [SerializeField] GameObject currentAmmo;
->>>>>>> cd2ae05d26b2e8f0cdf5d3eaa14a2f23bd73fc4d
+
 
     float moveSpeed_base = 4;
     void Start()
     {
         body = gameObject.GetComponent<Rigidbody2D>();
-<<<<<<< HEAD
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Movement
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * moveSpeed_base;
-=======
-        ChangeAmmo(1);
     }
 
 
     void Update()
     {
-        
-        
         
         //movement
         body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * moveSpeed_base;
@@ -49,10 +37,11 @@ public class PlayerBasicControls : MonoBehaviour
             clone.layer = 7;
             
         }
+        ChangeAmmo(1);
     }
     public void ChangeAmmo(int type)
     {
         currentAmmo = GameObject.Find("Bullet " + type.ToString());
->>>>>>> cd2ae05d26b2e8f0cdf5d3eaa14a2f23bd73fc4d
+
     }
 }
