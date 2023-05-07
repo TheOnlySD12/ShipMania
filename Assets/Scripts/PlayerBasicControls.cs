c4f7aa5e0f90fb1cb40d3f3318cac2aee20be5d1
Rares_Hapaianu <rares.hapaianu@gmail.com>
2023-05-07 19:10:59 +0300

    plus

--- Assets/Scripts/PlayerBasicControls.cs
+++ Assets/Scripts/PlayerBasicControls.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerBasicControls : MonoBehaviour
 {
     Rigidbody2D body;
+<<<<<<< HEAD
+=======
+    [SerializeField] float gunTimer;
+    float firerate = 1; //cu cat e mai mare, cu atat mai repede trage
+    GameObject clone;
+    [SerializeField] GameObject currentAmmo;
+>>>>>>> cd2ae05d26b2e8f0cdf5d3eaa14a2f23bd73fc4d
 
     float moveSpeed_base = 4;
     void Start()
     {
         body = gameObject.GetComponent<Rigidbody2D>();
+<<<<<<< HEAD
     }
 
     // Update is called once per frame
@@ -17,5 +25,34 @@ public class PlayerBasicControls : MonoBehaviour
     {
         //Movement
         body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * moveSpeed_base;
+=======
+        ChangeAmmo(1);
+    }
+
+
+    void Update()
+    {
+        
+        
+        
+        //movement
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * moveSpeed_base;
+
+        //lupta
+        gunTimer -= Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && gunTimer < 0)
+        {
+            gunTimer = 1 / firerate;
+            clone = Instantiate(currentAmmo, body.position, new Quaternion());
+            clone.GetComponent<Rigidbody2D>().velocity = Vector2.up * currentAmmo.GetComponent<BulletBehaviour>().bulletSpeed;
+            clone.layer = 7;
+            
+        }
+    }
+    public void ChangeAmmo(int type)
+    {
+        currentAmmo = GameObject.Find("Bullet " + type.ToString());
+>>>>>>> cd2ae05d26b2e8f0cdf5d3eaa14a2f23bd73fc4d
     }
 }
