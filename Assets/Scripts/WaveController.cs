7b3291d3051fca2a730c7117852dd9512319452f
Burrito910 <103515044+Burrito910@users.noreply.github.com>
2023-05-09 22:27:54 +0300

    enemyuri. in prgres. de fct merge

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    float spawningTimer = 0;
+    float spawningRate;
+
+    int enemiesAlive = 0;
+    int enemiesLeftToSpawn;
+    GameObject enemyObject;
+    int waveNumber = 0;
+    GameObject[] waypointList;
+
+    int numberOfWaveParents;
+    GameObject[] waveParentArray;
+    void Start()
+    {
+        waypointList = GameObject.FindGameObjectsWithTag("Waypoint");
+        foreach(GameObject waypoint in waypointList)
+        {
+            //waypoint.SetActive(false);
+        }
+
+        for (int x = 0; x < numberOfWaveParents; x++)  // toate waypointurile active de pe scena sunt puse, in ordine, intr-un array.
+        {
+            waveParentArray[x] = GameObject.Find("Wave " + (x+1) + " Parent");
+        }
+
+        foreach(GameObject parent in waveParentArray)
+        {
+            parent.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        spawningTimer -= Time.deltaTime;
+        
+        if(enemiesAlive == 0)
+        {
+            waveParentArray[waveNumber].SetActive(false);
+
+            waveNumber++;
+
+            waveParentArray[waveNumber].SetActive(true);
+            
+            if (waveNumber == 1) { spawningRate = 1; enemiesLeftToSpawn = 12; enemyObject = GameObject.Find("Enemy 1"); }
+
+            
+        }
+        
+        if(spawningTimer <= 0 && enemiesLeftToSpawn > 0)
+        {
+            spawningTimer = spawningRate;
+            Instantiate(enemyObject, transform.position, new Quaternion());
+            
+        }
+        
+        enemiesAlive = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+    
+    
+}
